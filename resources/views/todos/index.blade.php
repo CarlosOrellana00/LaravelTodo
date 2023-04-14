57b16b653b5b5c759a7f7d38e2449a722259679f
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-04-14 13:44:53 -0400

    messages success & alert

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,6 +4,12 @@
   <div class="container w-25 border p-4 mt-4">
       <form action="{{route('todos')}}" method="POST">
         @csrf
+        @if (session('success'))
+            <h6 class="alert alert-success">{{ session('sucess') }}</h6>
+        @endif
+        @error('title')
+            <h6 class="alert alert-danger">{{$message}}</h6>
+        @enderror
         <div class="mb-3">
           <label for="title" class="form-label">Titulo de la Tarea</label>
           <input type="text" name="title" class="form-control">
